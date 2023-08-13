b8c1d14cd74883ca8997aa4fb4378b4275a8e867
VVasilevich <vve913@gmail.com>
2023-08-13 22:04:04 +0600

    Add solution 8 kyu

--- solutions/8-kyu/Tag Arrays/solutions.js
+++ solutions/8-kyu/Tag Arrays/solutions.js
@@ -64,3 +64,10 @@ function fakeBin(x) {
 function stringToArray(string) {
   return string.split(' ');
 }
+
+// Sentence Smash
+// https://www.codewars.com/kata/53dc23c68a0c93699800041d
+
+function smash (words) {
+  return words.join(' ');
+}
