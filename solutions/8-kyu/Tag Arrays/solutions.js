2a8e58f2eb58176564c94d8036deb4cc2df386ea
VVasilevich <vve913@gmail.com>
2023-08-17 23:10:25 +0600

    Add solution 8 kyu

--- solutions/8-kyu/Tag Arrays/solutions.js
+++ solutions/8-kyu/Tag Arrays/solutions.js
@@ -153,3 +153,11 @@ function squareSum(numbers) {
 function squareSum(numbers) {
   return numbers.reduce((a, b) => a + b * b, 0);
 }
+
+// Filter out the geese
+// https://www.codewars.com/kata/57ee4a67108d3fd9eb0000e7
+
+function gooseFilter (birds) {
+  var geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
+  return birds.filter(item => !geese.includes(item));
+}
