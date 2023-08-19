1ca3bf4e7aca2d718e376d055dbe70589d4b73e6
VVasilevich <vve913@gmail.com>
2023-08-19 18:20:13 +0600

    Add solution 8 kyu

--- solutions/8-kyu/Tag Arrays/solutions.js
+++ solutions/8-kyu/Tag Arrays/solutions.js
@@ -168,3 +168,10 @@ function gooseFilter (birds) {
 function printArray(array) {
   return array.join(',');
 }
+
+// Get the mean of an array
+// https://www.codewars.com/kata/563e320cee5dddcf77000158
+
+function getAverage(marks) {
+  return Math.floor(marks.reduce((a, b) => a + b) / marks.length);
+}
