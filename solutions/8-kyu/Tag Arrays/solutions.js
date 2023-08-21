9a0f274fc94afb9e77874df8ca3a9b9d17ed13db
VVasilevich <vve913@gmail.com>
2023-08-21 19:12:09 +0600

    Add solution 8 kyu

--- solutions/8-kyu/Tag Arrays/solutions.js
+++ solutions/8-kyu/Tag Arrays/solutions.js
@@ -197,3 +197,10 @@ function countPositivesSumNegatives(input) {
   
   return [pos, neg];
 }
+
+// My head is at the wrong end!
+// https://www.codewars.com/kata/56f699cd9400f5b7d8000b55
+
+function fixTheMeerkat(arr) {
+  return arr.reverse();
+}
